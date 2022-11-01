0dc61d94c742ba6db0d97b9b53ec0bcc35fa7967
vigneshc98 <vignesh.c1698@gmail.com>
2022-11-02 00:38:56 +0530

    vn:288-A25

---
+++ Intellij-MicroservicesGuru/mssc-brewery-eureka/src/main/java/guru/sfg/brewery/eureka/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package guru.sfg.brewery.eureka.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
